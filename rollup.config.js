d577f7c5c4e79100e08ca75621fbb0c4839cef6a
modernator <rico345100@gmail.com>
2019-03-30 19:06:59 +0900

    Added uglify on production build

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,24 @@
 import babel from 'rollup-plugin-babel';
+import { uglify } from 'rollup-plugin-uglify';
+
+const isDev = process.env.BUILD !== 'production';
+const plugins = [
+    babel({
+        exclude: 'node_modules/**'
+    })
+];
+
+if(!isDev) {
+    plugins.push(uglify());
+}
 
 export default {
     input: 'src/index.js',
     output: {
         file: 'build/bundle.js',
         format: 'umd',
-        name: 'myModule'
+        name: 'myModule',
+        sourcemap: isDev ? true : false
     },
-    plugins: [
-        babel({
-            exclude: 'node_modules/**'
-        })
-    ]
+    plugins
 };
